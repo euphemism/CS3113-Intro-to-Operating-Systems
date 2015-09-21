916035dd7af09b6d07cb568e68d10b2f7477173f
Nicholas Sidwell <nicholas.sidwell@gmail.com>
2015-09-21 17:22:49 -0500

    Started writing method to read lines.

--- Homework 2/main.c
+++ Homework 2/main.c
@@ -1,8 +1,44 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER_SIZE 1024
+
+char* read_line(char* buffer, size_t length, FILE* f)
+{
+	
+	char* line;
+	
+	if (line = fgets(buffer, length, f))
+	{
+		
+		size_t last = strlen(buffer) - 1;
+		
+		if (buffer[last] == '\n')
+		{
+			
+			buffer[last] = '\0';
+		}
+		else
+		{
+			
+			fscanf(f, "%*[^\n]");
+			(void) fgetc(f);
+		}
+	}
+	
+	return line;
+}
+
 int main(void)
 {
-	printf("Hello, World!\n");
+
+	char buffer[BUFFER_SIZE];
+	char *input;
+	for (int i = 0; i < 10; i++)
+	{
+		
+		input = read_line(buffer, BUFFER_SIZE,  stdin);
+		printf(input + '\n');
+	}
 	exit(0);
 }
