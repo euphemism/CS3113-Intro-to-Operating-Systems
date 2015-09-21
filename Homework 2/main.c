dfa2bdf62bcdbcdd4143327b6bc4db1d23077e2e
Nicholas Sidwell <nicholas.sidwell@gmail.com>
2015-09-21 18:26:08 -0500

    Started work on string exploder.

--- Homework 2/main.c
+++ Homework 2/main.c
@@ -5,6 +5,9 @@
 #define BUFFER_SIZE 1024
 
 char* read_line(char* buffer, size_t length, FILE* f)
+/*
+	From the tutorial at http://home.datacomm.ch/t_wolf/tw/c/getting_input.html
+*/
 {
 	
 	char* line;
@@ -30,18 +33,55 @@ char* read_line(char* buffer, size_t length, FILE* f)
 	return line;
 }
  
+ char** explode(char* str, char delimiter)
+ {
+	 char** array = malloc(sizeof(char*));
+	 int max_size = strlen(str);
+	 int current_size = 0;
+	 int offset = 0;
+	 
+	 for (int i = 0; i < max_size; i++)
+	 {
+		 
+		 char* token = (char*) malloc(max_size * sizeof(char));
+		 
+		 if (str[i] == delimiter)
+		 {
+
+			 current_size++;
+			 array = realloc(array, current_size * sizeof(char*));
+			 array[current_size - 1] = malloc(((i - offset) + 2) * sizeof(char));
+			 
+			 for (int j = 0; j < (i - offset); j++)
+			 {
+				 
+				 array[current_size - 1][j] = str[i - offset + j];
+			 }
+			 
+			 offset = i + 1;
+		 }
+	 }
+	 
+	 return array;
+ }
+ 
 int main(void)
 {
 
 	char buffer[BUFFER_SIZE];
 	char *input;
 
-	for (int i = 0; i < 10; i++)
-	{
-		input = read_line(buffer, BUFFER_SIZE,  stdin);
-		printf(input);
-		free(input);
-	}
+	input = read_line(buffer, BUFFER_SIZE,  stdin);
+	char** tokens = explode(input, ' ');
+	printf(input);
 	
+	for (int i = 0; i < sizeof(tokens) / sizeof(char*); i++)
+	{
+		
+		printf(tokens[i]);
+		printf('%');
+		
+	}
+
 	exit(0); 
 }
